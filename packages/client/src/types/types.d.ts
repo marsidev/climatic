bb22eb85531fd75848217f2c62063d0aaf966538
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-22 20:02:10 -0400

    chore(types): :label: update tsconfig options, import server types on client

--- packages/client/src/types/types.d.ts
+++ packages/client/src/types/types.d.ts
@@ -3,3 +3,5 @@ export type GeoPermission = 'granted' | 'denied' | 'prompt'
 export type GeoStatus = 'loading' | 'not_supported' | 'error' | 'success' | null
 
 export type GeoPosition = number | null
+
+export * from '@server/types/responses'
