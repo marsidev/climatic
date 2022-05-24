8786c6487d197dc9bf0a9ef7745dc20dbd5da39f
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-24 18:53:45 -0400

    feat(layout): :sparkles: add app layout

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -2,34 +2,25 @@ import type { FlexProps } from '@chakra-ui/react'
 import type { ForecastResponse } from '@types'
 
 import { FC } from 'react'
-import { Flex } from '@chakra-ui/react'
 import GeoPermissionButton from '@components/GeoPermissionButton'
 import MainWeather from '@components/MainWeather'
 import Forecast from '@components/Forecast'
+import Layout from '@components/Layout'
 import useGeo from '@hooks/useGeo'
 import useForecast from '@hooks/useForecast'
 
-const App: FC<FlexProps> = ({ ...props }) => {
+const App: FC<FlexProps> = () => {
   const geoData = useGeo()
   const forecastData = useForecast(geoData) as ForecastResponse
 
   if (!forecastData) return null
 
   return (
-    <Flex
-      as='main'
-      bg='blue.300'
-      color='white'
-      display='flex'
-      flexDir='column'
-      justify='flex-start'
-      minH='100vh'
-      {...props}
-    >
+    <Layout>
       <GeoPermissionButton />
       <MainWeather data={forecastData} />
       <Forecast data={forecastData} mt={8} />
-    </Flex>
+    </Layout>
   )
 }
 
---
+++ packages/client/src/components/Layout.tsx
@@ -0,0 +1,30 @@
+import type { FlexProps } from '@chakra-ui/react'
+import { FC } from 'react'
+import { Flex } from '@chakra-ui/react'
+
+const Layout: FC<FlexProps> = ({ children }) => {
+  return (
+    <Flex
+      align='center'
+      as='main'
+      bg='purple.300'
+      color='white'
+      flexDir='column'
+      justify='flex-start'
+      minH='100vh'
+    >
+      <Flex
+        // border='2px solid purple'
+        flexDir='column'
+        h='full'
+        justify='flex-start'
+        maxW={500}
+        w='100%'
+      >
+        {children}
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Layout
