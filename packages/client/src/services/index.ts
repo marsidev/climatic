bbd4e655ce0df305d755ccd4c8981fcfaf0e18a4
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-23 20:28:50 -0400

    refactor(api): :zap: rename api endpoints

--- packages/client/src/services/index.ts
+++ packages/client/src/services/index.ts
@@ -1,7 +1,7 @@
 import type { ForecastResponse } from '@types'
 
 export const getForecast = async (): Promise<ForecastResponse> => {
-  const url = '/api/get-forecast?days=8'
+  const url = '/api/forecast?days=8'
   const data = await fetch(url).then(r => r.json())
   return data
 }
--- packages/server/src/routing.ts
+++ packages/server/src/routing.ts
@@ -4,8 +4,8 @@ import { ping, getWeather, getForecast } from '@controllers/index'
 const routing = (server: FastifyInstance): void => {
   const { register } = server
   register(ping, { prefix: '/api/ping' })
-  register(getWeather, { prefix: '/api/get-weather' })
-  register(getForecast, { prefix: '/api/get-forecast' })
+  register(getWeather, { prefix: '/api/weather' })
+  register(getForecast, { prefix: '/api/forecast' })
 
   server.after(err => err ? console.log(err) : console.log('Routes are ready.'))
 }
