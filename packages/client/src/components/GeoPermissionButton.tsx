3903c39dd9a762422b42376d4acef354b6da2b9d
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-25 20:15:27 -0400

    perf(components): :zap: allow recieve props from parent

--- packages/client/src/components/GeoPermissionButton.tsx
+++ packages/client/src/components/GeoPermissionButton.tsx
@@ -3,14 +3,14 @@ import { Flex, FlexProps, IconButton } from '@chakra-ui/react'
 import { MdMyLocation as GeoIcon } from 'react-icons/md'
 import useGeo from '@hooks/useGeo'
 
-const GeoPermissionButton: FC<FlexProps> = () => {
+const GeoPermissionButton: FC<FlexProps> = ({ ...props }) => {
   const { grantPermission, status } = useGeo()
   const showGeoButton = status === 'error' || status === 'not_supported' || status === 'denied'
 
   if (!showGeoButton) return null
 
   return (
-    <Flex border='1px solid' flexDir='row' justify='flex-end' >
+    <Flex flexDir='row' justify='flex-end' pr={2} {...props}>
       <IconButton
         aria-label='Grant geolocation permission'
         borderRadius='full'
