0ee80526ff7399ff430a36f0e16654dccab38615
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-25 20:14:39 -0400

    feat: :heavy_plus_sign: add react-toastify

--- packages/client/src/main.tsx
+++ packages/client/src/main.tsx
@@ -1,20 +1,39 @@
-import 'isomorphic-fetch'
 import '@styles/index.css'
+import 'react-toastify/dist/ReactToastify.css'
+import 'isomorphic-fetch'
 import theme from '@lib/theme'
-import App from '@/App'
+import App from '@components/App'
 import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
+import { Zoom, ToastContainer, ToastContainerProps } from 'react-toastify'
 
 let container: any = null
+
+const toastProps: ToastContainerProps = {
+  position: 'top-center',
+  autoClose: 1000,
+  hideProgressBar: true,
+  newestOnTop: false,
+  closeOnClick: true,
+  rtl: false,
+  pauseOnFocusLoss: false,
+  draggable: true,
+  pauseOnHover: false,
+  theme: 'dark',
+  limit: 15,
+  closeButton: false,
+  transition: Zoom,
+  icon: false
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   if (!container) {
     container = document.getElementById('root') as HTMLElement
     const root = createRoot(container)
-
     root.render(
       <ChakraProvider theme={theme}>
         <App />
+        <ToastContainer {...toastProps} />
       </ChakraProvider>
     )
   }
