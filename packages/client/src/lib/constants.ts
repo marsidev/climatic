7acd9d5e8e913d69daf5b9ec124a297899a77ed4
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-31 15:17:49 -0400

    perf: :sparkles: add ability to retrieve mock data in client if SHOW_MOCK_DATA_ON_DEV=true

--- packages/client/src/lib/constants.ts
+++ packages/client/src/lib/constants.ts
@@ -1,6 +1,10 @@
 import type { Coordinates } from '@types'
 
+export const SHOW_MOCK_DATA_ON_DEV = true
+
 export const DEFAULT_LOCATION: Coordinates = {
   latitude: 41.3879,
   longitude: 2.1699
 }
+
+export const DISABLED_TIMEOUT: number = 1000
--- packages/client/src/services/forecast.ts
+++ packages/client/src/services/forecast.ts
@@ -1,29 +1,45 @@
-import type { Location } from '@types'
 import type { ForecastResponse } from '@climatic/shared'
-import { DEFAULT_LOCATION } from '@lib/constants'
+import { DEFAULT_LOCATION, SHOW_MOCK_DATA_ON_DEV } from '@lib/constants'
+import { Coordinates, LocationStatus } from '@types'
 
-export const getForecast = async (locationData: Location): Promise<ForecastResponse> => {
-  const { coords, status } = locationData
+interface GetForecast {
+  coords: Coordinates | null
+  locationStatus: LocationStatus
+}
+
+export const getForecast = async ({ coords, locationStatus }: GetForecast): Promise<ForecastResponse> => {
   const latitude = coords?.latitude
   const longitude = coords?.longitude
 
   const noCoords: boolean = !latitude && !longitude
-  const noGeo: boolean = status === 'denied' || status === 'not_supported' || status === 'error'
+  const noGeo: boolean = locationStatus === 'denied' || locationStatus === 'not_supported' || locationStatus === 'error'
   let lat: string
   let lon: string
+  let url: string
 
-  if (noGeo || noCoords) {
-    console.log('no geo, fetching default forecast location')
-    lat = DEFAULT_LOCATION.latitude?.toString() ?? ''
-    lon = DEFAULT_LOCATION.longitude?.toString() ?? ''
+  if (SHOW_MOCK_DATA_ON_DEV) {
+    if (noGeo || noCoords) {
+      console.log('retrieving default mock data')
+      url = '/api/forecast?q=mock_BCN'
+    } else {
+      console.log('retrieving mock data with location')
+      url = '/api/forecast?q=mock_LA'
+    }
   } else {
-    lat = latitude?.toString() ?? ''
-    lon = longitude?.toString() ?? ''
-  }
+    if (noGeo || noCoords) {
+      console.log('no coords, fetching default forecast location')
+      lat = DEFAULT_LOCATION.latitude?.toString() ?? ''
+      lon = DEFAULT_LOCATION.longitude?.toString() ?? ''
+    } else {
+      console.log('fetching custom location forecast')
+      lat = latitude?.toString() ?? ''
+      lon = longitude?.toString() ?? ''
+    }
 
-  const params = { q: `${lat},${lon}`, days: '8' }
-  const queryString = new URLSearchParams(params).toString()
-  const url: string = `/api/forecast?${queryString}`
+    const params = { q: `${lat},${lon}`, days: '8' }
+    const queryString = new URLSearchParams(params).toString()
+    url = `/api/forecast?${queryString}`
+  }
 
   console.log({ url })
   const data = await fetch(url).then(r => r.json())
