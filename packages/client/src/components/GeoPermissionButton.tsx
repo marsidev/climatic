617efc38a0d640dd10b393c88985ae7ba601c838
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-31 15:20:45 -0400

    show an alert instead just a button + implement zustand

--- packages/client/src/components/GeoPermissionButton.tsx
+++ packages/client/src/components/GeoPermissionButton.tsx
@@ -1,25 +1,68 @@
-import { FC } from 'react'
-import { Flex, FlexProps, IconButton } from '@chakra-ui/react'
-import { MdMyLocation as GeoIcon } from 'react-icons/md'
-import { useLocation } from '@hooks'
+import { FC, useState, useEffect } from 'react'
+import { Alert, AlertIcon, FlexProps, HStack, IconButton, Spacer, Text } from '@chakra-ui/react'
+import { MdMyLocation as GeoIcon, MdClose as CloseIcon } from 'react-icons/md'
+import { useStore } from '@store'
 
 export const GeoPermissionButton: FC<FlexProps> = ({ ...props }) => {
-  const { grantPermission, status } = useLocation()
-  const showGeoButton = status === 'error' || status === 'not_supported' || status === 'denied'
+  const { grantPermission, locationStatus, isSupported, permission } = useStore()
+  const [showAlert, setShowAlert] = useState(false)
+
+  const showGeoButton =
+    locationStatus !== 'success'
+    && locationStatus !== 'denied'
+    && isSupported
+    && showAlert
+
+  useEffect(() => {
+    if (permission === 'prompt' && locationStatus === 'idle') {
+      setShowAlert(true)
+    }
+  }, [locationStatus, permission])
+
+  const hideAlert = () => setShowAlert(false)
 
   if (!showGeoButton) return null
 
   return (
-    <Flex flexDir='row' justify='flex-end' pr={2} {...props}>
-      <IconButton
-        aria-label='Grant geolocation permission'
-        borderRadius='full'
-        colorScheme='pink'
-        icon={<GeoIcon />}
-        variant='solid'
-        onClick={grantPermission}
-      />
-    </Flex>
+    <Alert
+      borderRadius={8}
+      color='#000'
+      flexDir='row'
+      px={2}
+      status='warning'
+      variant='left-accent'
+      {...props}
+    >
+      <AlertIcon />
+
+      <Text fontSize={12}>
+        Acepta el permiso de geolocalización para obtener el clima de tu ubicación.
+      </Text>
+
+      <Spacer />
+
+      <HStack>
+        <IconButton
+          aria-label='Grant geolocation permission'
+          borderRadius={9999}
+          colorScheme='green'
+          icon={<GeoIcon />}
+          size='sm'
+          variant='solid'
+          onClick={grantPermission}
+        />
+
+        <IconButton
+          aria-label='Close alert'
+          borderRadius={9999}
+          colorScheme='red'
+          icon={<CloseIcon />}
+          size='sm'
+          variant='solid'
+          onClick={hideAlert}
+        />
+      </HStack>
+    </Alert>
   )
 }
 
