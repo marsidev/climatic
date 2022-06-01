9eaa40f9db944024c6b3483df47a2e797670b2e1
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-05-31 22:55:39 -0400

    feat(pages): :sparkles: change page title dinamically based on weather data

--- packages/client/src/hooks/useForecast.ts
+++ packages/client/src/hooks/useForecast.ts
@@ -1,6 +1,7 @@
-import type { ForecastResponse } from '@climatic/shared'
+import type { ForecastResponse, TemperatureUnit } from '@climatic/shared'
 import { useEffect } from 'react'
 import { useStore } from '@store'
+import { formatTemperature } from '@lib/intl'
 
 type ReturnState = ForecastResponse | null
 
@@ -12,6 +13,22 @@ export const useForecast = (): ReturnState => {
       getForecastData({ coords, locationStatus })
     }
   }, [locationStatus])
+
+  useEffect(() => {
+    if (forecastData) {
+      const { location, currentWeather } = forecastData
+      const { temperature, isDay } = currentWeather
+
+      const unit: TemperatureUnit = 'celsius'
+      const _temperature = temperature[unit]
+      const temperatureString = formatTemperature(_temperature, unit)
+      const city = location.name
+      const timeEmoji = isDay ? '☀' : '🌕'
+
+      const title = `${temperatureString} en ${city} ${timeEmoji} | Climatic`
+      document.title = title
+    }
+  }, [forecastData])
 
   return forecastData
 }
