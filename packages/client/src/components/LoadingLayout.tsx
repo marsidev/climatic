8f7602ee77cf45148d0bd42a37db3bd46480d6eb
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-06-02 13:59:06 -0400

    style(components): :lipstick: improve skeleton loading page to fit with main layout

--- packages/client/src/components/LoadingLayout.tsx
+++ packages/client/src/components/LoadingLayout.tsx
@@ -1,31 +1,36 @@
 import { FlexProps } from '@chakra-ui/react'
 import { FC } from 'react'
-import { Flex, Heading, Skeleton, VStack } from '@chakra-ui/react'
+import { Box, Heading, HStack, Skeleton, VStack } from '@chakra-ui/react'
 
 export const LoadingLayout: FC<FlexProps> = () => {
   return (
-    <Flex
-      align='center'
-      as='main'
-      // bg='purple.300'
-      color='white'
-      flexDir='column'
-      justify='flex-start'
-      minH='100vh'
-    >
-      <Flex flexDir='column' maxW={500} w='100%'>
+    <Box className='container'>
+      <Box as='main' className='weather-card'>
         <VStack align='center' flexDir='column' mt={4}>
           <Skeleton w='90%'>
-            <Heading as='h2' fontSize={48}>Location</Heading>
+            <Heading as='h2' fontSize={48}>
+              Location
+            </Heading>
           </Skeleton>
 
           <Skeleton w='90%'>
-            <Heading as='h3' fontSize={24} lineHeight={1}>Condition</Heading>
+            <Heading as='h3' fontSize={24} lineHeight={1}>
+              Condition
+            </Heading>
           </Skeleton>
 
           <Skeleton w='40%'>
-            <Heading as='h3' fontSize={96} lineHeight={1}>20</Heading>
+            <Heading as='h3' fontSize={96} lineHeight={1}>
+              20
+            </Heading>
           </Skeleton>
+
+          <HStack align='center' as='ul' justify='center' pt={8} w='90%'>
+            <Skeleton as='li' borderRadius={12} height='75px' w='20%' />
+            <Skeleton as='li' borderRadius={12} height='75px' w='20%' />
+            <Skeleton as='li' borderRadius={12} height='75px' w='20%' />
+            <Skeleton as='li' borderRadius={12} height='75px' w='20%' />
+          </HStack>
         </VStack>
 
         <VStack gap={2} mt={8}>
@@ -37,8 +42,8 @@ export const LoadingLayout: FC<FlexProps> = () => {
           <Skeleton height='30px' w='90%' />
           <Skeleton height='30px' w='90%' />
         </VStack>
-      </Flex>
-    </Flex>
+      </Box>
+    </Box>
   )
 }
 
