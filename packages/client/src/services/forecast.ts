316ecb4e783946c8455663b261dc6eaebfed03ce
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-06-02 22:50:50 -0400

    perf: :recycle: refactor and cleanup code

--- packages/client/src/services/forecast.ts
+++ packages/client/src/services/forecast.ts
@@ -12,47 +12,36 @@ export interface GetForecastByQuery {
   query: string
 }
 
-const ENVIROMENT: any = process.env.NODE_ENV
+const ENVIRONMENT: any = process.env.NODE_ENV
+const DEV_MODE: boolean = ENVIRONMENT === 'development'
+const SHOW_MOCK: boolean = SHOW_MOCK_DATA_ON_DEV && DEV_MODE
+const DEFAULT_FORECAST_DAYS = '8'
 
 export const fetchForecastByCoords = async ({ coords, locationStatus }: GetForecastByCoords): Promise<ForecastResponse> => {
-  const latitude = coords?.latitude
-  const longitude = coords?.longitude
+  const { latitude, longitude } = coords ?? {}
+  const { latitude: defaultLat, longitude: defaultLon } = DEFAULT_LOCATION
 
   const noCoords: boolean = !latitude && !longitude
   const noGeo: boolean = locationStatus === 'denied' || locationStatus === 'not_supported' || locationStatus === 'error'
-  let lat: string
-  let lon: string
-  let url: string
+  const validLocation = noGeo || noCoords
 
-  if (SHOW_MOCK_DATA_ON_DEV && ENVIROMENT === 'development') {
-    if (noGeo || noCoords) {
-      url = '/api/forecast?q=mock-bcn'
-    } else {
-      url = '/api/forecast?q=mock-la'
-    }
-  } else {
-    if (noGeo || noCoords) {
-      lat = DEFAULT_LOCATION.latitude?.toString() ?? ''
-      lon = DEFAULT_LOCATION.longitude?.toString() ?? ''
-    } else {
-      lat = latitude?.toString() ?? ''
-      lon = longitude?.toString() ?? ''
-    }
+  let query: string = ''
+  if (SHOW_MOCK && validLocation) query = 'mock-la'
+  if (SHOW_MOCK && !validLocation) query = 'mock-bcn'
+  if (!SHOW_MOCK && validLocation) query = `${defaultLat},${defaultLon}`
+  if (!SHOW_MOCK && !validLocation) query = `${latitude},${longitude}`
 
-    const params = { q: `${lat},${lon}`, days: '8' }
-    const queryString = new URLSearchParams(params).toString()
-    url = `/api/forecast?${queryString}`
-  }
-
-  const data = await fetch(url).then(r => r.json())
-  return data
-}
-
-export const fetchForecastByQuery = async ({ query }: GetForecastByQuery): Promise<ForecastResponse> => {
-  const params = { q: query, days: '8' }
+  const params = { q: query, days: DEFAULT_FORECAST_DAYS }
   const queryString = new URLSearchParams(params).toString()
   const url = `/api/forecast?${queryString}`
 
-  const data = await fetch(url).then(r => r.json())
-  return data
+  return fetch(url).then(r => r.json())
+}
+
+export const fetchForecastByQuery = async ({ query }: GetForecastByQuery): Promise<ForecastResponse> => {
+  const params = { q: query, days: DEFAULT_FORECAST_DAYS }
+  const queryString = new URLSearchParams(params).toString()
+  const url = `/api/forecast?${queryString}`
+
+  return fetch(url).then(r => r.json())
 }
