4fd352d0b6adc763752f6ecf9d654a08e966625a
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-06-06 19:11:05 -0400

    perf: :zap: improve caching - cache also content-type

--- packages/server/src/plugins/cache.ts
+++ packages/server/src/plugins/cache.ts
@@ -1,46 +1,77 @@
-import type { FastifyPluginAsync } from 'fastify'
-import type { Key, Options } from 'node-cache'
+import type { FastifyPluginAsync, FastifyRequest } from 'fastify'
+import type { Key } from 'node-cache'
 
 import fp from 'fastify-plugin'
 import NodeCache from 'node-cache'
 
+const DEBUG = false
 const DEFAULT_CACHE_TTL: number = 900 // cache duration in seconds - 15 minutes
 const SEARCH_CACHE_TTL: number = 86400 // 24h
-let CACHE_KEY: Key
 
-const CACHE_OPTIONS: Options = {
+const cacheInstance = new NodeCache({
   stdTTL: DEFAULT_CACHE_TTL,
   checkperiod: DEFAULT_CACHE_TTL * 2
+})
+
+const getCacheKeys = (request: FastifyRequest): Key[] => {
+  const { url, method } = request
+  const key1 = `${method}__${url}`
+  const key2 = `${key1}__content-type`
+  return [key1, key2]
 }
-const cacheInstance = new NodeCache(CACHE_OPTIONS)
 
-const pluginCallback: FastifyPluginAsync = async (server, _options) => {
-  server.addHook('onRequest', async (request, reply) => {
-    if (request.method === 'GET') {
-      const { url, method } = request
-      CACHE_KEY = `${method}-${url}`
-      const cached = cacheInstance.get(CACHE_KEY)
+const pluginCallback: FastifyPluginAsync = async (fastify, _options) => {
+  fastify.addHook('onRequest', async (request, reply) => {
+    const { url, method } = request
+    if (method === 'GET') {
+      const keys = getCacheKeys(request)
 
-      if (cached !== undefined) {
-        // console.log('RETURNING FROM CACHE FOR KEY', CACHE_KEY)
-        reply.send(cached)
+      const cached = cacheInstance.get(keys[0])
+      const cachedType = cacheInstance.get(keys[1])
+      const isCached = cached !== undefined && cachedType !== undefined
+
+      if (isCached) {
+        if (DEBUG) {
+          console.log({
+            message: 'returns from cache',
+            url,
+            cachedType,
+            keys
+          })
+        }
+
+        reply.type(cachedType as string).send(cached)
       }
     }
   })
 
-  server.addHook('onSend', (request, reply, payload, done) => {
-    if (request.method === 'GET') {
-      const { url, method } = request
-      CACHE_KEY = `${method}-${url}`
-      const response = cacheInstance.get(CACHE_KEY)
+  fastify.addHook('onSend', (request, reply, payload, done) => {
+    const { url, method } = request
+    if (method === 'GET') {
+      const contentType = reply.getHeader('content-type')
+      const keys = getCacheKeys(request)
 
-      if (url.substring(0, 5) === '/api/') {
-        if (response === undefined && reply.statusCode < 400) {
-          // console.log('CACHING RESPONSE FOR KEY', CACHE_KEY)
-          if (url?.includes('/api/search?q=')) {
-            cacheInstance.set(CACHE_KEY, payload, SEARCH_CACHE_TTL)
+      const cached = cacheInstance.get(keys[0])
+      const cachedType = cacheInstance.get(keys[1])
+      const isCached = cached !== undefined && cachedType !== undefined
+
+      if (url.startsWith('/api/')) {
+        if (!isCached && reply.statusCode < 400) {
+          if (DEBUG) {
+            console.log({
+              message: 'caching response',
+              url,
+              contentType,
+              keys
+            })
+          }
+
+          if (url?.startsWith('/api/search?q=')) {
+            cacheInstance.set(keys[0], payload, SEARCH_CACHE_TTL)
+            cacheInstance.set(keys[1], contentType, SEARCH_CACHE_TTL)
           } else {
-            cacheInstance.set(CACHE_KEY, payload, DEFAULT_CACHE_TTL)
+            cacheInstance.set(keys[0], payload, DEFAULT_CACHE_TTL)
+            cacheInstance.set(keys[1], contentType, DEFAULT_CACHE_TTL)
           }
         }
       }
