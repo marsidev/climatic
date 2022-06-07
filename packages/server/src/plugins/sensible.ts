763329515e2dd82a9e2747a480749f169abc93ae
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-06-07 17:39:47 -0400

    feat(api): :sparkles: add sensible plugin

---
+++ packages/server/src/plugins/sensible.ts
@@ -0,0 +1,8 @@
+import fp from 'fastify-plugin'
+import sensible, { SensibleOptions } from '@fastify/sensible'
+
+export default fp<SensibleOptions>(async (fastify, _opts) => {
+  fastify.register(sensible, {
+    errorHandler: false
+  })
+})
