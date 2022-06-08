153481b56bccaddf829473a57efbc09716c78416
Luis Marsiglia <luismarsiglia19@gmail.com>
2022-06-07 22:27:19 -0400

    perf(api): :technologist: enable pino logger

--- packages/server/src/lib/logger.ts
+++ packages/server/src/lib/logger.ts
@@ -1,12 +1,25 @@
 import pino from 'pino'
 
+// https://github.com/pinojs/pino/blob/master/docs/api.md#options-object
 export const logger = pino({
+  serializers: {
+    req(request) {
+      const { url, method } = request
+      return `${method} - ${url}`
+    },
+    res(response) {
+      const { statusCode } = response
+      return `${statusCode}`
+    }
+  },
+  name: 'server',
+  level: 'info',
   transport: {
     target: 'pino-pretty',
     options: {
       colorize: true,
-      ignore: 'pid,hostname',
-      translateTime: true
+      translateTime: true,
+      ignore: 'hostname,pid'
     }
   }
 })
--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,9 +1,10 @@
 import buildApp from '@app'
 import config from '@lib/config'
+import { logger } from '@lib'
 
 const runServer = async () => {
   const { PORT, HOST } = config
-  const app = await buildApp({ logger: false })
+  const app = await buildApp({ logger })
 
   app.listen(PORT, HOST, async (err, address) => {
     if (err) return console.error(err)
