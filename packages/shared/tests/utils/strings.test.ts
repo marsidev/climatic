ae44b813759f34cbd82e1a2fc1132928147d75b4
Luis Marsiglia <marsiglia.business@gmail.com>
2022-06-15 16:53:48 -0400

    feat(tests): :sparkles: replace jest with vitest in shared package

--- packages/shared/tests/utils/strings.test.ts
+++ packages/shared/tests/utils/strings.test.ts
@@ -1,57 +1,62 @@
+import { describe, it, expect } from 'vitest'
 import '../../src/utils/strings'
 
 describe('string prototype', () => {
   describe('replaceSpaces method', () => {
-    describe('without replaceWith value', () => {
-      it('can remove spaces', () => {
-        const [str, replaceWith, expected] = ['New York', undefined, 'NewYork']
-        const result = str.replaceSpaces(replaceWith)
-        expect(result).toEqual(expected)
-      })
+    it('can remove spaces', () => {
+      const [str, replaceWith, expected] = ['New York', undefined, 'NewYork']
+      const result = str.replaceSpaces(replaceWith)
+      expect(result).toEqual(expected)
     })
 
-    describe('with replaceWith value', () => {
-      it('can replace spaces', () => {
-        const [str, replaceWith, expected] = ['New York', '-', 'New-York']
-        const result = str.replaceSpaces(replaceWith)
-        expect(result).toEqual(expected)
-      })
+    it('can replace spaces', () => {
+      const [str, replaceWith, expected] = ['New York', '-', 'New-York']
+      const result = str.replaceSpaces(replaceWith)
+      expect(result).toEqual(expected)
     })
   })
 
   describe('replaceExtraSpaces method', () => {
-    describe('without replaceWith value', () => {
-      it('can remove extra spaces', () => {
-        const [str, replaceWith, expected] = ['new             york', '', 'newyork']
-        const result = str.replaceSpaces(replaceWith)
-        expect(result).toEqual(expected)
-      })
+    it('can remove extra spaces', () => {
+      const [str, replaceWith, expected] = [
+        'new             york',
+        '',
+        'newyork'
+      ]
+      const result = str.replaceSpaces(replaceWith)
+      expect(result).toEqual(expected)
     })
 
-    describe('with replaceWith value', () => {
-      it('can replace extra spaces', () => {
-        const [str, replaceWith, expected] = ['new             york', '-', 'new-------------york']
-        const result = str.replaceSpaces(replaceWith)
-        expect(result).toEqual(expected)
-      })
+    it('can replace extra spaces', () => {
+      const [str, replaceWith, expected] = [
+        'new             york',
+        '-',
+        'new-------------york'
+      ]
+      const result = str.replaceSpaces(replaceWith)
+      expect(result).toEqual(expected)
     })
   })
 
   describe('replaceSpecialChars method', () => {
-    describe('without replaceWith value', () => {
-      it('can remove', () => {
-        const [str, replaceWith, expected] = ['---new ?/york++', undefined, 'new york']
-        const result = str.replaceSpecialChars(replaceWith)
-        expect(result).toEqual(expected)
-      })
+    it('can remove', () => {
+      const [str, replaceWith, expected] = [
+        '---new ?/york++',
+        undefined,
+        'new york'
+      ]
+      const result = str.replaceSpecialChars(replaceWith)
+      expect(result).toEqual(expected)
     })
 
-    describe('with replaceWith value', () => {
-      it('can replace', () => {
-        const [str, replaceWith, expected] = ['---new ?/york++', ' ', '   new   york  ']
-        const result = str.replaceSpecialChars(replaceWith)
-        expect(result).toEqual(expected)
-      })
+    it('can replace', () => {
+      const [str, replaceWith, expected] = [
+        '---new ?/york++',
+        ' ',
+        '   new   york  '
+      ]
+      const result = str.replaceSpecialChars(replaceWith)
+      expect(result).toEqual(expected)
     })
   })
 
