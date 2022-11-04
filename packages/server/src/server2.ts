ab62f283e6ba8659eb132a7b979e3d540fb63e67
Luis Marsiglia <marsiglia.business@gmail.com>
2022-11-03 23:25:09 -0400

    prevent top level await

--- packages/server/src/server2.ts
+++ packages/server/src/server2.ts
@@ -36,27 +36,31 @@ import { logger as pino, startupLog } from './lib/logger'
 // runServer()
 // export default runServer
 
-const startTime = performance.now()
-const { PORT: port, HOST: host } = config
+const runServer = async () => {
+	const startTime = performance.now()
+	const { PORT: port, HOST: host } = config
 
-const args = process.argv.slice(2)
-const showLogger = args.includes('--log')
-const expose = args.includes('--host')
-const logger = showLogger ? pino : false
+	const args = process.argv.slice(2)
+	const showLogger = args.includes('--log')
+	const expose = args.includes('--host')
+	const logger = showLogger ? pino : false
 
-const app = await buildApp({ logger })
-// const app = Fastify({ logger })
+	const app = await buildApp({ logger })
+	// const app = Fastify({ logger })
 
-// await app.register(autoload, {
-// 	dir: join(__dirname, 'plugins')
-// })
+	// await app.register(autoload, {
+	// 	dir: join(__dirname, 'plugins')
+	// })
 
-// await app.register(autoload, {
-// 	dir: join(__dirname, 'routes'),
-// 	prefix: '/api'
-// })
+	// await app.register(autoload, {
+	// 	dir: join(__dirname, 'routes'),
+	// 	prefix: '/api'
+	// })
 
-app.listen({ port, host }, (err, address) => {
-	if (err) return console.error(err)
-	if (process.env.NODE_ENV !== 'production') startupLog(address, startTime, expose)
-})
+	app.listen({ port, host }, (err, address) => {
+		if (err) return console.error(err)
+		if (process.env.NODE_ENV !== 'production') startupLog(address, startTime, expose)
+	})
+}
+
+runServer()
